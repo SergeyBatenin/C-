96ba412aeaca72a0b6e7a4473a6d6fb3c21ac00d
SergeyBatenin <inferno.87@mail.ru>
2022-03-22 21:29:21 +0300

    AddTaskSem4 № 1,2

---
+++ HomeWork01/AddTaskSem4/Task01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+class Program
+{
+
+    /*
+    На вход подаётся натуральное десятичное число. Проверьте, является ли оно палиндромом в двоичной записи
+
+    Пример:
+    341 = 101010101 (является)
+    45 = 101101 (является)
+    56 = 111000 (Не является)
+    */
+
+    static void Main()
+    {
+        int decNumder = Convert.ToInt32(Console.ReadLine());
+        int binNumber = Convert.ToInt32(DecToBin(decNumder));
+        Palindrom(binNumber);
+    }
+    static string DecToBin(int num)
+    {
+        string binNumber = "";
+        if (num <= 0)
+        {
+            return binNumber;
+        }
+
+        while (num != 0)
+        {
+            binNumber = num % 2 + binNumber;
+            num = num / 2;
+        }
+        return binNumber;
+    }
+    static void Palindrom(int num)
+    {
+        //считаем длину числа
+        int count = 0;
+        int temp = num;
+        while (temp > 0)
+        {
+            temp = temp / 10;
+            count++;
+        }
+        bool test = true; //флаг конечного результата
+
+        for (int i = 0; i < count / 2; i++)
+        {
+            // Вычисляем цифры попарно и сравниваем их
+            int startPow = (int)Math.Pow(10, (count - 1 - i));
+            int startDigit = num / startPow % 10;
+            int endPow = (int)Math.Pow(10, i);
+            int endDigit = num / endPow % 10;
+
+            if (startDigit != endDigit)
+            {
+                Console.WriteLine($"Число {num}  не является палиндромом");
+                test = false;
+                break;
+            }
+
+        }
+        if (test == true) Console.WriteLine($"Число {num} является палиндромом");
+    }
+}
---
+++ HomeWork01/AddTaskSem4/Task02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+class Program
+{
+    /*
+    Напишите метод, который заполняет массив случайным количеством (от 1 до 100) 
+    нулей и единиц. Размер массива должен совпадать с количеством единиц в нём
+    */
+
+    static void Main()
+    {
+        int[] array;
+        array = FillArray();
+        PrintArray(array);
+    }
+
+    static int[] FillArray()
+    {
+        Random rnd = new Random();
+        string s = string.Empty;
+        int count = 0;
+        int i = 0;
+        bool flag = false;
+        while (!flag)
+        {
+            i++;
+            int temp = rnd.Next(2);
+            s = s + temp;
+            if (temp == 1)
+            {
+                count++;
+            }
+            if (count * count == i)
+            {
+                flag = true;
+            }
+
+            /*
+            * По сути проверкой ниже мы сразу проверяем условие задачи,
+            * массив у нас будет длиной от 1 ДО 100, иначе псевдослучайная
+            * генерация массива у нас не удалась и возвращаем пустой массив
+            */
+
+            if (count > 9 || i == 100)
+            {
+                Console.WriteLine("Количество элементов со значением 1 сгенерировалось больше,\n" +
+                                "поэтому создать массив под заданные условия не представляется возможным");
+
+                return new int[0];
+            }
+        }
+
+        int[] array = new int[count * count];
+        for (int j = 0; j < array.Length; j++)
+        {
+            array[j] = Convert.ToInt32(s[j].ToString());
+        }
+        
+        return array;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
