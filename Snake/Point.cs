e75acd21fe79dceb793d276544683092aa2e3b97
SergeyBatenin <inferno.87@mail.ru>
2022-03-25 23:06:25 +0300

    Create class Point

---
+++ Snake/Point.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake;
+class Program
+{
+    static void Main()
+    {
+        Point p1 = new Point();
+        p1.x = 4;
+        p1.y = 6;
+        p1.sym = '+';
+        p1.Draw();
+    }    
+}
