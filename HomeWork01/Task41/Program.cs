6a1ed66db14ba594b8843703c8985f25af63feb4
SergeyBatenin <inferno.87@mail.ru>
2022-03-29 14:45:48 +0300

    Homework Seminar 6

---
+++ HomeWork01/Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    /*
+    * Пользователь вводит с клавиатуры M чисел. 
+    * Посчитайте, сколько чисел больше 0 ввёл пользователь.
+    */
+    static void Main()
+    {
+        int total = 7;
+        //int total = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        for (int i = 0; i < total; i++)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0) count++;
+        }
+        Console.WriteLine($"Пользователь ввел {count} чисел больше 0");
+    }
+}
---
+++ HomeWork01/Task43/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class Program
+{
+    /*
+    * Напишите программу, которая найдёт точку пересечения двух прямых,
+    * заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+    * значения b1, k1, b2 и k2 задаются пользователем.
+    */
+    static void Main()
+    {
+        double b1 = 2;
+        double k1 = 5;
+        double b2 = 4;
+        double k2 = 9;
+        //double b1 = Convert.Todouble(Console.ReadLine());
+        //double k1 = Convert.Todouble(Console.ReadLine());
+        //double b2 = Convert.Todouble(Console.ReadLine());
+        //double k2 = Convert.Todouble(Console.ReadLine());
+
+        double x, y;
+
+        if (k1 == k2)
+        {
+            if (b1 == b2)
+            {
+                Console.WriteLine("Прямые совпадают");
+            }
+            else
+            {
+                Console.WriteLine("Прямые параллельны");
+            }
+        }
+        else
+        {
+            /*
+            * исходя из условий составим равенство
+            * k1 * x + b1 = k2 * x + b2    отсюда
+            * k1 * x - k2 * x = b2 - b1
+            * (k1 - k2) * x = b2 - b1
+            * x = (b2 - b1) / (k1 - k2)
+            */
+            
+            x = (b2 - b1) / (k1 - k2);
+
+            // Соответственно, чтобы найти y подставляем 
+            // значение х в одно из уравнений прямой
+
+            y = k1 * x + b1;
+
+            Console.WriteLine($" Прямые заданные данными уравнениями пересекаются в точке с координатами ({x}, {y})");
+        }
+
+    }
+}
