2dca10a4bddb3fc37c4dc0f363e1228f3db26cf8
SergeyBatenin <inferno.87@mail.ru>
2022-03-31 00:10:18 +0300

    AddTasks Seminar 6

---
+++ HomeWork01/AddTaskSem06/Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    /*
+    * Написать перевод десятичного числа в двоичное, используя рекурсию.
+    */
+    static void Main()
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(DecToBin(number));
+    }
+
+    static string DecToBin(int n)
+    {   
+        if (n == 0) return "";
+        if (n == 1) return "1";
+        //string result = n % 2 + "";
+        //result = n % 2 + result;          
+        
+        return DecToBin(n/2) + n % 2 + "";
+    }
+}
---
+++ HomeWork01/AddTaskSem06/Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    /*
+    * Дано число N. Используя рекурсию, определите,
+    *  что оно является степенью числа M
+    */
+    static void Main()
+    {
+        double n = Convert.ToDouble(Console.ReadLine());
+        int degree = Convert.ToInt32(Console.ReadLine());
+
+        if (PowThree(n, degree)) Console.WriteLine("Число является степенью тройки");
+        else Console.WriteLine("Число НЕ является степенью тройки");
+    }
+    
+    static bool PowThree(double n, int degree)
+    {
+        if (n == degree) return true;
+        else if (n > degree) return PowThree(n / degree, degree);
+        else return false;
+    }
+}
