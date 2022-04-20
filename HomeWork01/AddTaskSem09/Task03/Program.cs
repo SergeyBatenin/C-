091578fe1a04b34bf330eff8a904592201ae052e
SergeyBatenin <inferno.87@mail.ru>
2022-04-20 09:25:47 +0300

    Доделана допЗадача №3 Семинара 9

--- HomeWork01/AddTaskSem09/Task03/Program.cs
+++ HomeWork01/AddTaskSem09/Task03/Program.cs
@@ -10,26 +10,39 @@ class Program
 
     static void Main()
     {
-        int a = 5;
-        int b = 3;
+        int a = 2;
+        int b = 7;
         //Console.WriteLine(RecursivePow(a, b));
-        RecursivePow(a,b);
+        RecursivePow(a,b,a);
     }
 
-    static void RecursivePow(int num, int power,int number = 0, int count = 1)
+    static void RecursivePow(int num, int power,int number, int result = 0, int count = 1)
     {
-        if (count == power) return;
-        
-        for (int i = 0; i < num; i++)
+        if (power == 0)
         {
-            number = number + num;
+            Console.WriteLine($"Число {num} в степени {power} равно = 1");
+            return;
         }
-        Console.WriteLine(number);
-        RecursivePow(num, power, number, ++count);
+        if (power == 1)
+        {
+            Console.WriteLine($"Число {num} в степени {power} равно = {num}");
+            return;
+        }
+        if (count == power)
+        {
+            Console.WriteLine($"Число {num} в степени {power} равно = {number}");
+            return;
+        }
+
+        int counterAdd = num * number;
+        for (int i = 0; i < counterAdd; i++)
+        {
+            result++;            
+        }        
+        RecursivePow(num, power, result, 0, ++count);
     }
 }
 /*
-
 We can calculate power by using repeated addition. 
 For example to calculate 5^6. 
 1) First 5 times add 5, we get 25. (5^2) 
@@ -37,5 +50,4 @@ For example to calculate 5^6.
 3) Then 5 times add 125, we get 625 (5^4) 
 4) Then 5 times add 625, we get 3125 (5^5) 
 5) Then 5 times add 3125, we get 15625 (5^6)
-
 */
