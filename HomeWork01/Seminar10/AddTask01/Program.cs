b3c8ca2257bcc09b7072b2ac978f178b102fc004
SergeyBatenin <inferno.87@mail.ru>
2022-04-22 08:23:07 +0300

    ДопЗадачи №№ 1, 2 к Семинару 10

---
+++ HomeWork01/Seminar10/AddTask01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    /*
+    * Даны два числа a, b. Сложите их, используя
+    * только операции инкремента и декремента.
+    */
+
+    // Если я правильно понял условие, то вроде так))
+    static void Main()
+    {
+        int firstNumber = -4;
+        int secondNumber = 5;
+        
+        int sum = SumTwoNumbers(firstNumber, secondNumber);
+
+        Console.WriteLine(sum);
+    }
+
+    static int SumTwoNumbers(int first, int second)
+    {
+        if (second > 0)
+        {
+            while( second > 0)
+            {
+                first++;
+                second--;
+            }
+        }
+        else
+        {
+            while (second < 0)
+            {
+                first--;
+                second++;
+            }
+        }
+
+        return first;
+    }
+}
---
+++ HomeWork01/Seminar10/AddTask02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class Program
+{
+    /*
+    * Дана последовательность натуральных чисел.
+    * Определите значение второго по величине элемента в этой последовательности.
+    *  {1, 2, 3, 4, 5} >> 4
+    *  {7, 4, 2, 7, 6, 6, 8} >> 7
+    */
+    static void Main()
+    {
+        //string sequence = Console.ReadLine()?? "0";  
+        string sequence = "7, 4, 2, 9, 6, 6, 8";
+        int[] array = StringToArray(sequence);
+        PrintArray(array);
+        Console.WriteLine($"Вариант №1\nВторой по величине элемент равен {FindSecondMax(array)}\n");
+        
+        Array.Sort(array);
+        PrintArray(array);
+        Console.WriteLine();
+        Console.WriteLine($"Вариант №2\nВторой по величине элемент в последовательности равен {array[array.Length-2]}");
+    }
+
+    static int[] StringToArray(string inputData)
+    {
+        int[] result = inputData.Replace(',', ' ')
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(Int32.Parse)
+                                .ToArray();
+        return result;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    static int FindSecondMax(int[] array)
+    {
+        int length = array.Length;
+        if (length == 0) return -1;
+        int max = array[0];
+        int secondMax = array[0];
+
+        for (int i = 1; i < length; i++)
+        {
+            if (array[i] >= max)
+            {
+                secondMax = max;
+                max = array[i];
+            }
+            else if (array[i] > secondMax) secondMax = array[i];
+        }
+
+        return secondMax;
+    }
+}
